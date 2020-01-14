f1e090ba3f2007cbecf5f7c58ba7d8a59458ec71
timelord <timemonster666@gmail.com>
2020-01-15 02:09:04 +0500

    Add files

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TelegramBot
+{
+
+
+
+
+    class Program
+    {
+       static Dictionary<string, string> questions;
+        
+
+        static void Main(string[] args)
+        {
+
+            var Api = new TelegramAPI();
+            var data = System.IO.File.ReadAllText(@"C:\Users\timelord\source\repos\TelegramBot\TelegramBot\Properties\questions.json");
+            questions = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(data);
+
+            while (true)
+            {
+                var updates =  Api.getUpdates();
+
+                foreach(var up in updates)
+                {
+                    var answer = AnswerQuestion(up.message.text);
+                    Api.sendMessage(answer, up.message.chat.id);
+                    
+                }
+
+                
+
+            }
+
+
+
+
+
+
+
+
+        }
+
+        static String AnswerQuestion(String question)
+        {
+            List<string> answers = new List<string>();
+
+            question = question.ToLower();
+            foreach(var q in questions)
+            {
+                if (question.Contains(q.Key))
+                {
+                    answers.Add(q.Value);
+                }
+            }
+
+            if (answers.Count == 0)
+            {
+                answers.Add("моя твоя не понимать");
+            }
+        
+
+            
+
+
+         
+              return  String.Join(", ", answers);
+          
+
+         
+
+
+
+        }
+    }
+}
---
+++ TelegramBot/TelegramAPI.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace TelegramBot
+{
+
+
+    class Chat
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+    }
+
+    class Message
+    {
+       public Chat chat { get; set; }
+        public string text { get; set; }
+    }
+
+    class Update
+    {
+        public int update_id { get; set; }
+        public Message message { get; set; }
+    }
+
+
+    class ApiResult
+    {
+        public Update[] result { get; set; }
+    }
+
+
+
+
+    class TelegramAPI
+    {
+        const String API_KEY = "877006364:AAGavRTDRBIzOoxOS3GRaBWVJ9E0Wsui_CQ";
+        const String API_URL = "https://api.telegram.org/bot" + API_KEY + "/";
+
+        RestClient RC = new RestClient();
+
+        public TelegramAPI()
+        {
+
+        }
+
+        public Update[] getUpdates()
+        {
+            var json = sendApiRequest("getUpdates", "offset=" + lastUpdateId);
+            var updates = JsonConvert.DeserializeObject<ApiResult>(json);
+
+            foreach(var result in updates.result)
+            {
+                Console.WriteLine("recievd update" + result.update_id + "message from  " + result.message.chat.first_name + "Text: "  + result.message.text);
+                lastUpdateId = result.update_id + 1;
+            }
+            return updates.result;
+        }
+
+
+        public void sendMessage(String text, int id)
+        {
+            sendApiRequest("sendMessage", $"chat_id={id}&text={text}");
+            
+        }
+
+
+        public String sendApiRequest(String ApiMethod)
+        {
+            var url = API_URL + ApiMethod;
+            var request = new RestRequest(url);
+            var response = RC.Get(request);
+
+            return response.Content;
+        }
+
+
+        public String sendApiRequest(String ApiMethod, String parametrs)
+        {
+            var url = API_URL + ApiMethod + "?" + parametrs;
+            var request = new RestRequest(url);
+            var response = RC.Get(request);
+            
+
+            return response.Content;
+        }
+
+        private int lastUpdateId = 0;
+
+    }
+}
